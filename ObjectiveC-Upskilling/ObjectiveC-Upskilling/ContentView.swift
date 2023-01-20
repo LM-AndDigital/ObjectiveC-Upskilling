3e262cfd7b633ed7c48b93eba4d2f113d2a2703a
Hamzah Khan <hamzah.khan@and.digital>
2023-01-20 11:40:32 +0000

    Project Start

---
+++ ObjectiveC-Upskilling/ObjectiveC-Upskilling/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ObjectiveC-Upskilling
+//
+//  Created by Hamzah Khan on 20/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ObjectiveC-Upskilling/ObjectiveC-Upskilling/ObjectiveC_UpskillingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectiveC_UpskillingApp.swift
+//  ObjectiveC-Upskilling
+//
+//  Created by Hamzah Khan on 20/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ObjectiveC_UpskillingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
